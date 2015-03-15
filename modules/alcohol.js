02210badfbfe2116179f57c089353a5c8a127f6a
Teddy Wing <git@teddywing.com>
2015-03-14 23:03:07 -0400

    Add alcohol.js reader
    Gets data from a sensor which measures alcohol presence in the air. A
    bit tricky with the calculations. Still not sure what the numbers
    actually mean but we're getting numbers. Hey.
    
    Just copied over useful parts from the Arduino example.

---
+++ modules/alcohol.js
@@ -0,0 +1,37 @@
+var mraa = require('mraa');
+
+module.exports = function(emitter, pin) {
+        var Alcohol = {
+                scan: function() {
+                        var sensor = new mraa.Aio(pin || 2);
+                        return this.alcohol_reading(sensor);
+                },
+
+                // Based on:
+                // https://github.com/Seeed-Studio/Grove_Alcohol_Sensor/blob/masster/DetectAlcoholVapor.ino
+                alcohol_reading: function(sensor) {
+                        var reading = sensor.read();
+                        var compensator = 1023;
+                        var value = compensator - reading;
+
+                        if (value < 200) {
+                                return 0;
+                        }
+                        else {
+                                this.to_percentage(value);
+                        }
+                },
+
+                to_percentage: function(value) {
+                        var high_alcohol_concentration = 750;
+                        var factor_of_nonexistence = 200;
+                        return (value - factor_of_nonexistence) * 100 / high_alcohol_concentration;
+                }
+        };
+
+        emitter.emit('register', 'alcohol');
+
+        setInterval(function() {
+                emitter.emit('alcohol', Alcohol.scan());
+        }, 5000);
+};
