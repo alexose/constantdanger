f65431d19b1a9ccad19261716e8419828b514e38
Alexander Ose <alex@alexose.com>
2015-03-14 21:45:37 -0400

    fix temp bug

--- modules/temp.js
+++ modules/temp.js
@@ -3,7 +3,6 @@ var groveSensor = require('jsupm_grove');
 
 // Create the temperature sensor object using AIO pin 0
 var temp = new groveSensor.GroveTemp(0);
-console.log(temp.name());
 
 // Measure temperature and report on it
 module.exports = function(emitter){
@@ -13,19 +12,20 @@ module.exports = function(emitter){
 
   // Read the temperature ten times, printing both the Celsius and
   // equivalent Fahrenheit temperature, waiting one second between readings
+
   setInterval(function(){
 
-    var temp = celsius * 9.0/5.0 + 32.0
+    var f = temp.value() * 9.0/5.0 + 32.0
       , value;
 
     // Broadcast a value on excessive temperature
-    if (temp > 90){
+    if (f > 90){
       var value =  Math.min((temp - 90) * 2, 100);
       emitter.emit('excessivetemperature', value);
     }
 
     // Broadcast a value on super-duper low temperature
-    if (temp < 20){
+    if (f < 20){
       var value = Math.min((temp - 40) * -2, 100)
       emitter.emit('lowtemperature', value);
     }
