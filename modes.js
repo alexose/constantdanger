7ce64f48eaaa4b844311484fd49283089f66b8f5
Alexander Ose <alex@alexose.com>
2015-03-14 22:01:17 -0400

    add air quality to modes index

--- modes.js
+++ modes.js
@@ -3,19 +3,21 @@
 module.exports = {
     general : {
         entropy : 40,
-        airquality: 30
+        air_quality: 30
     },
     bodily_harm : {
         excessivetemperature: 20, // Temp > 100
         lowtemperature:       20,  // Temp < 20
         gasleak:              80,
         angrysportsfans:      20,
+        air_quality:          10,
         crime:                35
     },
     emotional_harm : {
         angrysportsfans:      10,
         stockmarketdown:      20,
         hackathon:            40,
+        air_quality:          20,
         nowifi:               90
     },
 }
