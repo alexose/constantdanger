2fbc57fde99e512f605a2aa95969b2892ff04255
Alexander Ose <alex@alexose.com>
2015-03-14 21:59:41 -0400

    add general mode

--- modes.js
+++ modes.js
@@ -1,6 +1,10 @@
 // Modes!
 // Ideally, each observation has a 'relevance score' between 0 and 100.
 module.exports = {
+    general : {
+        entropy : 40,
+        airquality: 30
+    },
     bodily_harm : {
         excessivetemperature: 20, // Temp > 100
         lowtemperature:       20,  // Temp < 20
