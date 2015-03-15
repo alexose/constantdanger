897b717a8cbde10ec50647319a4aa72e5f43f3f8
Alexander Ose <alex@alexose.com>
2015-03-14 20:10:14 -0400

    fix comma

--- modes.js
+++ modes.js
@@ -3,7 +3,7 @@
 module.exports = {
     bodily_harm : {
         excessivetemperature: 20, // Temp > 100
-        lowtemperature:       20  // Temp < 20
+        lowtemperature:       20,  // Temp < 20
         gasleak:              80,
         angrysportsfans:      20,
         crime:                35
